a8d7ceb450b9aad66eb1c93340af5b73539df8b0
Ivan Silva <silvaivan280@gmail.com>
2023-08-04 20:52:43 -0300

    agregue card en el componente DetalleProducto

--- src/views/DetalleProducto.jsx
+++ src/views/DetalleProducto.jsx
@@ -1,9 +1,10 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const DetalleProducto = () => {
   return (
-    <Container className='my-4 main font'>
-        <h3>Detalle Producto</h3>
+    <Container className="my-4 main font">
+      <h3>Detalle Producto</h3>
       <Card className="p-3">
         <Row>
           <Col>
@@ -24,6 +25,65 @@ const DetalleProducto = () => {
           </Col>
         </Row>
       </Card>
+      <div className="my-5">
+        <h3>Más Productos</h3>
+      <Row className="mx-5">
+        <Col md={4}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://images.pexels.com/photos/3829226/pexels-photo-3829226.jpeg"
+            />
+            <Card.Body className="font">
+              <Card.Title>Teclado Gamers</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Link className="btn btn-outline-primary" to={"/detalle"}>
+                Ver más...
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://images.pexels.com/photos/3829226/pexels-photo-3829226.jpeg"
+            />
+            <Card.Body className="font">
+              <Card.Title>Teclado Gamers</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Link className="btn btn-outline-primary" to={"/detalle"}>
+                Ver más...
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://images.pexels.com/photos/3829226/pexels-photo-3829226.jpeg"
+            />
+            <Card.Body className="font">
+              <Card.Title>Teclado Gamers</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Link className="btn btn-outline-primary" to={"/detalle"}>
+                Ver más...
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </div>
     </Container>
   );
 };
